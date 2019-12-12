d462deccdcba03f9d88cb049fae8e8a6178994de
Valdir Coxev <valdircoxevdev@gmail.com>
2019-12-12 05:35:37 -0400

    ESLint, Prettier & EditorConfig

--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
-import express from 'express'
-import routes from './routes'
+import express from 'express';
+import routes from './routes';
 
-class App 
-{
-    constructor(){
-        this.serve = express()
-        this.middleware()
-        this.routes()
-    }
+class App {
+  constructor() {
+    this.serve = express();
+    this.middleware();
+    this.routes();
+  }
 
-    middleware(){
-        this.serve.use(express.json())
-    }
+  middleware() {
+    this.serve.use(express.json());
+  }
 
-    routes(){
-        this.serve.use(routes)
-    }
+  routes() {
+    this.serve.use(routes);
+  }
 }
 
-export default new App().serve
+export default new App().serve;
