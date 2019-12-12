9d61927ed2192c65a30ef97533bf70d78c91cbfb
Valdir Coxev <valdircoxevdev@gmail.com>
2019-12-12 05:55:59 -0400

    Configurando Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'sAL5yNZO',
+    database: 'gobarber',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
