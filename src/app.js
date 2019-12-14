486c303196caf99f5a7d487471b8cce129809967
Valdir Coxev <valdircoxevdev@gmail.com>
2019-12-14 05:46:33 -0400

    Criando loader de models

--- src/app.js
+++ src/app.js
@@ -1,20 +1,22 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 class App {
-  constructor() {
-    this.serve = express();
-    this.middleware();
-    this.routes();
-  }
+    constructor() {
+        this.serve = express();
+        this.middleware();
+        this.routes();
+    }
 
-  middleware() {
-    this.serve.use(express.json());
-  }
+    middleware() {
+        this.serve.use(express.json());
+    }
 
-  routes() {
-    this.serve.use(routes);
-  }
+    routes() {
+        this.serve.use(routes);
+    }
 }
 
 export default new App().serve;
