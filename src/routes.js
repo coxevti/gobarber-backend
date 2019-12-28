d09768afe13e52a6c75e2d75dbb05de5c8269529
Valdir Coxev <valdircoxevdev@gmail.com>
2019-12-28 16:51:45 -0400

    Listando agenda do prestador

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/authMiddleware';
 import multerConfig from './config/multer';
@@ -26,6 +27,7 @@ routes.use(authMiddleware);
 
 routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 routes.post(
     '/appointments',
     appointmentStoreValidation,
