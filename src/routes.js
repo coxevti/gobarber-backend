69d41515022320d5590a706ae6c97fff81b6c404
Valdir Coxev <valdircoxevdev@gmail.com>
2019-12-28 08:17:13 -0400

    Listagem de prestadores de serviço

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
+import { resolve } from 'path';
 import multer from 'multer';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/authMiddleware';
 import multerConfig from './config/multer';
@@ -22,5 +24,11 @@ routes.use(authMiddleware);
 
 routes.put('/users', userUpdateValidation, UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
+
+routes.use(
+    '/files',
+    express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
+);
 
 export default routes;
